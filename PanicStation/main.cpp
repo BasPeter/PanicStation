c126e21741884cd5603a9cbdc9a4899407a7f24b
BuildTools <unconfigured@null.spigotmc.org>
2020-09-19 14:17:45 +0200

    Initial Commit

---
+++ PanicStation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PanicStation
+//
+//  Created by Bas Peter Dijkema on 19/09/2020.
+//  Copyright © 2020 Bas Peter Dijkema. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
