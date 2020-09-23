388e590fece8174fedd293c3496e8470e9117fa3
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 15:37:22 +0200

    Panic Station Concept

---
+++ PanicStation/Renderer/Renderer.hpp
@@ -0,0 +1,43 @@
+//
+//  Renderer.hpp
+//  PanicStation
+//
+//  Created by Bas Peter Dijkema on 19/09/2020.
+//  Copyright © 2020 Bas Peter Dijkema. All rights reserved.
+//
+
+#ifndef Renderer_hpp
+#define Renderer_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include "Node.hpp"
+
+class Renderer
+{
+public:
+    Renderer(std::size_t screen_width, std::size_t screen_height);
+    ~Renderer();
+    
+    void Render(std::shared_ptr<Node> currentNode, std::vector<std::shared_ptr<Node>> nodes, SDL_Point goal);
+    void UpdateWindowTitle(int fps);
+    SDL_Color getReadableRGB(int x, int y);
+private:
+    
+    SDL_Window *_sdl_window;
+    SDL_Renderer *_sdl_renderer;
+    SDL_Texture * _background_texture;
+    SDL_Surface * _heightmap_surface;
+    
+    const std::size_t _screen_width;
+    const std::size_t _screen_height;
+    
+    SDL_Texture* LoadTexture(std::string path);
+    
+    Uint32 getPixel(int x, int y);
+    void RenderPointer(int x, int y, std::shared_ptr<Node> node);
+};
+
+#endif /* Renderer_hpp */
--- PanicStation/main.cpp
+++ PanicStation/main.cpp
@@ -5,11 +5,29 @@
 //  Created by Bas Peter Dijkema on 19/09/2020.
 //  Copyright © 2020 Bas Peter Dijkema. All rights reserved.
 //
-
 #include <iostream>
+#include "Game.hpp"
+#include "Controller.hpp"
+#include "Renderer.hpp"
+
+#include "Node.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    constexpr std::size_t kFramesPerSecond{60};
+    constexpr std::size_t kMsPerFrame{1000/kFramesPerSecond};
+    constexpr std::size_t kWindowWidth{600};
+    constexpr std::size_t kWindowHeight{600};
+    
+    // Init Renderer
+    Renderer renderer(kWindowWidth, kWindowHeight);
+    
+    //Init Controller
+    Controller controller(kWindowWidth, kWindowHeight, &renderer);
+
+    // Init and start Game
+    Game game;
+    game.Run(controller, renderer, kMsPerFrame);
+
     return 0;
 }
