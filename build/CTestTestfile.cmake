3b7f888249c092f6d437ef76d4aceca2c9d96287
BasPeter <baspeterdijkema@hotmail.com>
2020-09-24 10:58:38 +0000

    removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/Desktop/PanicStation
-# Build directory: /root/Desktop/PanicStation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
